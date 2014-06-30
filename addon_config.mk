bcb1349096a15c9527677cb80ba6dc22c3957d29
Kirk Shoop <kirk.shoop@microsoft.com>
2014-06-29 23:44:07 -0700

    Create addon_config.mk

---
+++ addon_config.mk
@@ -0,0 +1,10 @@
+meta:
+	ADDON_NAME = ofxRx
+	ADDON_DESCRIPTION = An addon that bridges with rxcpp. rxcpp provides algorithms for event composition.
+	ADDON_AUTHOR = kirkshoop
+	ADDON_TAGS = "reactive" "rxcpp" "events" "streams"
+	ADDON_URL = http://github.com/kirkshoop/ofxRx
+common:
+	# dependencies with other addons, a list of them separated by spaces 
+	# or use += in several lines
+	ADDON_DEPENDENCIES = ofxGUI
