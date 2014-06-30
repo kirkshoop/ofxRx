b090e894aecf7110783b6d4123c7cda71a691301
Kirk Shoop <kirk.shoop@microsoft.com>
2014-06-30 09:28:47 -0700

    Update addon_config.mk

--- addon_config.mk
+++ addon_config.mk
@@ -1,6 +1,6 @@
 meta:
 	ADDON_NAME = ofxRx
-	ADDON_DESCRIPTION = An addon that bridges with rxcpp. rxcpp provides algorithms for event composition.
+	ADDON_DESCRIPTION = REQUIRES C++11 support (https://github.com/openFrameworks-cpp11/openFrameworks). ofxRx is an addon that bridges with rxcpp. rxcpp provides algorithms for event composition.
 	ADDON_AUTHOR = kirkshoop
 	ADDON_TAGS = "reactive" "rxcpp" "events" "streams"
 	ADDON_URL = http://github.com/kirkshoop/ofxRx
